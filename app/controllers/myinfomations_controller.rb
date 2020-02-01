211275a4de6cc1af7ef4f11a094fe6442b551ead
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-02-01 23:00:19 +0900

    myinfomationまわり

---
+++ app/controllers/myinfomations_controller.rb
@@ -0,0 +1,30 @@
+class MyinfomationsController < ApplicationController
+  def index
+  end
+  def notification
+  end
+  def todo
+  end
+  def like
+  end
+  def news
+  end
+  def review
+  end
+  def great
+  end
+  def good
+  end
+  def poor
+  end
+  def support
+  end
+  def sales
+  end
+  def point
+  end
+  def profile
+  end
+  def identification
+  end
+end
---
+++ test/controllers/myinfomations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MyinfomationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
