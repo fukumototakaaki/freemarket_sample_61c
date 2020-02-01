6699e504a998525f80edb14b9d342ea033ce690e
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-02-01 23:02:18 +0900

    itemsコントローラまわり

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,16 @@ class ItemsController < ApplicationController
     @products_c = ["レディース", "メンズ", "家電・スマホ・カメラ", "おもちゃ・ホビー・グッズ"]
     @products_b = ["シャネル","ルイヴィトン","シュプリーム","ナイキ"]
   end
+  def new
+  end
+  def listing
+  end
+  def in_progress
+  end
+  def completed
+  end
+  def purchase
+  end
+  def purchased
+  end
 end
